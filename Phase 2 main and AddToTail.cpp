877e925229d46b3ced04082f07d6f53b1a30fd77
ylhuongz <ylhuongz@gmail.com>
2024-06-07 08:02:35 -0400

    Update Phase 2 main and AddToTail.cpp

--- Phase 2 main and AddToTail.cpp
+++ Phase 2 main and AddToTail.cpp
@@ -2,56 +2,57 @@
 using namespace std;
 #include <cstdlib>
 
-struct node{
+struct node {
 	int data;
-	node *next;
-	node *prev;
+	node* next;
+	node* prev;
 };
 
-int count=0;
-node *head=NULL;		//head of linked-list
-node *tail=NULL;		//end of linked-list
-node *current;			//curent node	
+int count = 0;
+node* head = NULL;		//head of linked-list
+node* tail = NULL;		//end of linked-list
+node* current;			//curent node	
 
-void AddToTail(int b){
-	node *ptr;					
-	ptr=new node;
-	ptr->data=b;
-	if (count==0){			//if there is no list yet, 
-		ptr->prev=NULL;		//we make the new entrys prev NULL,
-		head=ptr;			//and then we make head equal to the new entry
+void AddToTail(int b) {
+	node* ptr;
+	ptr = new node;
+	ptr->data = b;
+	if (count == 0) {			//if there is no list yet, 
+		ptr->prev = NULL;		//we make the new entrys prev NULL,
+		head = ptr;			//and then we make head equal to the new entry
 	}
-	else{					//else, if the linked list already has data
-		tail->next=ptr;		//the current tail's next becomes the new entry	
+	else {					//else, if the linked list already has data
+		tail->next = ptr;		//the current tail's next becomes the new entry	
 		ptr->prev = tail;
 	}
-							//to update the nodes now...	
-	ptr->next=NULL;			//we make the new entries next NULL
-	tail=ptr;				//then we update the tail to the new entry
-	count++;	
+	//to update the nodes now...	
+	ptr->next = NULL;			//we make the new entries next NULL
+	tail = ptr;				//then we update the tail to the new entry
+	count++;
 }
 
-void AddToHead(int a){
-	node *ptr;
-	ptr=new node;
-	ptr->data=a;
-	if (count==0){
-		ptr->next=NULL;
-		tail=ptr;
+void AddToHead(int a) {
+	node* ptr;
+	ptr = new node;
+	ptr->data = a;
+	if (count == 0) {
+		ptr->next = NULL;
+		tail = ptr;
 	}
-	else{
-		ptr->next=head;	
-		head->prev=ptr;					
+	else {
+		ptr->next = head;
+		head->prev = ptr;
 	}
-	ptr->prev=NULL;
-	head=ptr;
-	count++;			
+	ptr->prev = NULL;
+	head = ptr;
+	count++;
 }
-			
-void PrintElements(node* headptr, node* tailptr) {
-	node* currentptr = headptr;					//create new node that points to headptr
+
+void PrintElements() {
+	node* currentptr = head;					//create new node that points to headptr
+
 	while (currentptr != nullptr) {					//traverse through entire list
-		if (currentptr == tailptr) {				//check if it's at the end of the list
+		if (currentptr == tail) {				//check if it's at the end of the list
 			cout << currentptr->data << endl;		//print without comma and end with a new line
 			currentptr = currentptr->next;			//move to next node
 		}
@@ -59,41 +60,41 @@ void PrintElements(node* headptr, node* tailptr) {
 			cout << currentptr->data << ", ";		//if it's not at the end, print with comma and a space
 			currentptr = currentptr->next;			//move to next node
 		}
-
 	}
 }
-void PrintReverse(){
-    node* current = tail;
-    while (current) {
-        cout << current->data << ", ";
-        current = current->prev;
-    }
-    cout << endl;
-}	
-			
+
+void PrintReverse() {
+	node* current = tail;
+	while (current) {
+		cout << current->data << ", ";
+		current = current->prev;
+	}
+	cout << endl;
+}
+
 //MAIN
-int main(){
-	for (int i=0; i<15; i++){AddToTail(rand()%100);}
-	PrintElements(head, tail);
-	cout<<endl;
-	
+int main() {
+	for (int i = 0; i < 15; i++) { AddToTail(rand() % 100); }
+	PrintElements();
+	cout << endl;
+
 	int choice;
 	int num;
-	do{
-		cout<<"Enter your choice: Add a new element(1), Print(2), Print reverse(3), or exit(4): ";
-		cin>>choice;
-		if((choice==1) ||(choice==2)||(choice==3) || (choice==4){		
-			if (choice==1){
-				cout<<"Enter an integer: ";
-				cin>>num;
+	do {
+		cout << "Enter your choice: Add a new element(1), Print(2), Print reverse(3), or exit(4): ";
+		cin >> choice;
+		if ((choice == 1) || (choice == 2) || (choice == 3) || (choice == 4)) {
+			if (choice == 1) {
+				cout << "Enter an integer: ";
+				cin >> num;
 				AddToTail(num);
 			}
-			if (choice==2){PrintElements(head, tail);}
-			if (choice==3){PrintReverse();}
+			if (choice == 2) { PrintElements(); }
+			if (choice == 3) { PrintReverse(); }
 		}
-		else{cout<<"Your entry is invalid."<<endl;}
-	}while(choice!=4);
-	
-	return 0;
+		else { cout << "Your entry is invalid." << endl; }
+	} while (choice != 4);
+
+		return 0;
 
 }
